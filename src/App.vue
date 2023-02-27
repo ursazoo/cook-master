3383dd5e691c7d738474d0952d0a5e0b09c4b88b
ursazoo <iamzhoufei@gmail.com>
2023-02-27 23:04:50 +0800

    feat: 修改主页背景

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,8 @@
 <script setup lang="ts"></script>
 
 <template>
-  <!-- <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <nav>
-        {{ text }}
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/link" />
-      </nav>
-    </div>
-  </header> -->
-
   <div class="container">
-    <div class="background"></div>
+    <div class="background" />
     <RouterView />
   </div>
 </template>

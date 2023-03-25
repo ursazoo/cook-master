5118d8d19807dfa2fc496d1a31a238276d0d7111
秦锋 <iamzhoufei@gmail.com>
2023-03-25 23:46:18 +0800

    chore: 完成项目基础配置

--- app.vue
+++ app.vue
@@ -1,5 +1,39 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <NuxtPage />
   </div>
 </template>
+
+<style lang="scss">
+@import './styles/font.scss';
+
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.2s;
+}
+
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(0.5rem);
+}
+
+* {
+  font-family: FZBIAOYSJW;
+}
+
+html,
+body {
+  cursor: url('@/assets/images/spatula.png'), auto;
+
+  &:hover {
+    cursor: url('@/assets/images/spatula.png'), pointer !important;
+  }
+}
+
+.n-tabs .n-tabs-tab {
+  &:hover {
+    cursor: url('@/assets/images/spatula.png'), pointer !important;
+  }
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,44 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
   title: "Hello Nuxt",
   app: {
-    pageTransition: { name: 'page', mode: 'out-in' }
+    head: {
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+    },
+    pageTransition: { name: "page", mode: "out-in" },
   },
+  build: {
+    transpile:
+      process.env.NODE_ENV === "production"
+        ? [
+            "naive-ui",
+            "vueuc",
+            "@css-render/vue3-ssr",
+            "@juggle/resize-observer",
+          ]
+        : ["@juggle/resize-observer"],
+  },
+  vite: {
+    server: {
+      proxy: {
+        "/api": {
+          target: "http://localhost:3000",
+          changeOrigin: true,
+        },
+      },
+    },
+    optimizeDeps: {
+      include:
+        process.env.NODE_ENV === "development"
+          ? ["naive-ui", "vueuc", "date-fns-tz/esm/formatInTimeZone"]
+          : [],
+    },
+  },
+  modules: [
+    // pinia plugin
+    '@pinia/nuxt',
+  ],
+
 });
